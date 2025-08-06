7d89ce8b302a572b7adb0cf5d85912a2e3427176
diablo <progzone122@gmail.com>
2025-08-06 19:56:42 +0300

    fix patch_all.sh autism
    fix patch_all.sh autism
    
    fix patch_all.sh autism
    
    fix patch_all.sh autism

--- patch_all.sh
+++ patch_all.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-patch Makefile < patches/fuck-gcc-wrapper.patch
-patch arch/arm64/mm/proc.S < patches/procs-assembly.patch
-patch scripts/dtc-lexer.lex.c_shipped < extern-yylloc.patch
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+patch -l Makefile < "$DIR/patches/fuck-gcc-wrapper.patch"
+patch -l arch/arm64/mm/proc.S < "$DIR/patches/procs-assembly.patch"
+patch -l scripts/dtc/dtc-lexer.lex.c_shipped < "$DIR/patches/extern-yylloc.patch"
